cf6d63042bb79536eafead0979efb426baf250a4
kittyaer-cpu <kittyaer@kimo.com>
2026-06-15 21:00:42 +0800

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyC0sY6JZPytn_COOnaWtaSNPaz2rMxClqo",
-  authDomain: "da-store-672b6.firebaseapp.com",
-  projectId: "da-store-672b6",
-  storageBucket: "da-store-672b6.firebasestorage.app",
-  messagingSenderId: "112757046080",
-  appId: "1:112757046080:web:f0767f1cfd501246d198a5",
-  measurementId: "G-5G569476QP"
-};
